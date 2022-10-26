525aee3c60684b8f8c40b8e5d431c9dc2f89cbaa
Mehmet Güdük <mehmetguduk98@gmail.com>
2022-10-26 19:20:40 +0300

    master

--- src/utils/api/getSearch.js
+++ src/utils/api/getSearch.js
@@ -8,7 +8,11 @@ async function getSearch(string) {
   let response;
   await fetch(`http://api.tvmaze.com/search/shows?q=${encodeURIComponent(string)}`)
     .then(response => response.json())
-    .then(jsonData => { response = jsonData })
+    .then(jsonData => { 
+      
+      response = jsonData 
+      console.log(response)
+    })
     .catch(error => { response = error })
   return response
 };
